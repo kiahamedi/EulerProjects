9eae2f520efe4e505fee44ad90da303767b25fce
pascalcase <ipascalcase@gmail.com>
2020-02-18 00:24:06 +0330

    add problem description to py file

--- ep8.py
+++ ep8.py
@@ -6,6 +6,14 @@ Created on Mon Feb 17 22:37:05 2020
 @author: kia
 """
 
+'''
+Euler Project 8
+
+The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+
+Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+'''
+
 mystr = '''
 73167176531330624919225119674426574742355349194934
 96983520312774506326239578318016984801869478851843
