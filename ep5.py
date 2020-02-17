e39b04881fe99631a6b17b9de2da8fb01e3ca83f
pascalcase <ipascalcase@gmail.com>
2020-02-17 18:09:07 +0330

    Solved Euler Project 5

---
+++ ep5.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 17:03:03 2020
+
+@author: kia
+"""
+
+'''
+Euler Project 5
+
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+'''
+
+
+def checkForTrue(target):
+    for i in range(1,21):
+        if target % i != 0 :
+            return False
+        
+    return True
+
+
+counter = 1
+while True:
+    if checkForTrue(counter) == True:
+        print(counter)
+        break
+    else:
+        counter += 1
